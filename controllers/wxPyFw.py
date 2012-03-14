6543b8875a498c5c732c8e07231df85c685e1f6e
Giuseppe Tripoli <gtripoli@skynex.it>
2012-03-14 20:40:31 +0100

    init git

---
+++ controllers/wxPyFw.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Giuseppe Tripoli"
+__date__ ="$14-mar-2012 20.30.02$"
+
+if __name__ == "__main__":
+    print "Hello World"
---
+++ wxPyFw.py
@@ -0,0 +1,9 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Giuseppe Tripoli"
+__date__ ="$14-mar-2012 20.03.26$"
+
+if __name__ == 'main' :
+    
+    
